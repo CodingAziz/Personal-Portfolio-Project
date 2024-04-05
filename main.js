46681897c102045bd52f716dd2fcf196d77e8f15
random_coder <aziz.officewala2003@gmail.com>
2024-04-05 13:03:13 +0530

    portfolio project created

---
+++ main.js
@@ -0,0 +1,17 @@
+const icon = document.getElementById("linkedin");
+const icon1 = document.getElementById("github");
+const icon2 = document.getElementById("twitter");
+
+icon.addEventListener('click', () => {
+    window.location.href = "https://www.linkedin.com/in/aziz-officewala-837034256/";
+});
+
+icon1.addEventListener('click', () => {
+    window.location.href = "https://github.com/CodingAziz";
+});
+
+icon2.addEventListener('click', () => {
+    window.location.href = "https://twitter.com/Azizreck";
+});
+
+
